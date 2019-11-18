44422b4cec857d1968234f53c849666372fb6c87
annkoz <aniakozakova@gmail.com>
2019-11-18 22:05:24 +0200

    Deleted several files

--- Automation_2/src/Lesson6/A.java
+++
@@ -1,8 +0,0 @@
-package Lesson6;
-
-public class A extends B {
-
-    public void printName() {
-        System.out.println("Class A:");
-    }
-}
--- Automation_2/src/Lesson8/LightBox.java
+++
@@ -1,9 +0,0 @@
-package Lesson8;
-
-public class LightBox extends AbstractBox {
-
-    @Override
-    public void printProperties() {
-        System.out.println("This is light box!");
-    }
-}
